0c06c27df0af2cfe4fb02b172ff59c1886f778cb
ramad <ramad@LAXMIPRASANNA>
2024-09-24 14:15:40 +0530

    getters and setters

---
+++ src/com/edubridge/myapp1/model/Employee.java
@@ -0,0 +1,26 @@
+package com.edubridge.myapp1.model;
+
+public class Employee{
+	private int employeeId;
+	private String employeeName;
+	private Double employeeSalary;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public Double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(Double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+}
